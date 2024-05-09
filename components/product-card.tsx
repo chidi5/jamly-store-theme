e05487cb6685836c0e0953a1b03c0393fa56e160
chidi5 <joshdash.ji@gmail.com>
2024-05-09 13:54:38 +0100

    mobile preview

--- components/product-card.tsx
+++ components/product-card.tsx
@@ -40,7 +40,7 @@ const ProductCard = ({ data }: ProductCardProps) => {
           alt={data.name}
         />
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
-          <div className="flex gap-x-6 justify-center">
+          <div className="hidden sm:flex gap-x-6 justify-center">
             <IconButton
               onClick={onPreview}
               icon={<Expand size={20} className="text-gray-600" />}
